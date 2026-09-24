cc71c5879b011d3da235439b78f93a3f1a1d49c6
popixoxipop-collab <popixoxipop@gmail.com>
2026-09-25 00:57:05 +0900

    test(webgame-a): include schema validation in CI shim

--- test/webgame-static.test.mjs
+++ test/webgame-static.test.mjs
@@ -4,3 +4,4 @@
 await import('./webgame-scan/parser.test.mjs');
 await import('./webgame-scan/project-discovery.test.mjs');
 await import('./webgame-scan/multiplane.test.mjs');
+await import('./webgame-scan/schema.test.mjs');
