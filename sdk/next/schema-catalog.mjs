c10defe31830961c219e3f472f0e6b8fc04b29cb
popixoxipop-collab <popixoxipop@gmail.com>
2026-09-25 11:53:41 +0900

    T22: add SDK schema catalog

---
+++ sdk/next/schema-catalog.mjs
@@ -0,0 +1,17 @@
+export const SDK_SCHEMA_IDS = Object.freeze({
+	manifest: 'urn:sbf:adapter-sdk-manifest:1',
+	workerRequest: 'urn:sbf:adapter-worker-request:1',
+	workerResponse: 'urn:sbf:adapter-worker-response:1',
+	supportExplanation: 'urn:sbf:support-explanation:1',
+	taskPacket: 'urn:sbf:adapter-task-packet:1',
+	conformance: 'urn:sbf:adapter-sdk-conformance:1',
+});
+
+export const SDK_SCHEMA_FILES = Object.freeze({
+	manifest: 'schemas/adapter-sdk-manifest.schema.json',
+	workerRequest: 'schemas/adapter-worker-request.schema.json',
+	workerResponse: 'schemas/adapter-worker-response.schema.json',
+	supportExplanation: 'schemas/support-explanation.schema.json',
+	taskPacket: 'schemas/adapter-task-packet.schema.json',
+	conformance: 'schemas/adapter-sdk-conformance.schema.json',
+});
