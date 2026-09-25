fc85d8764e22a60ba69aa45e913e5e797419b8de
popixoxipop-collab <popixoxipop@gmail.com>
2026-09-25 09:30:57 +0900

    T22: add sdk/next/index.mjs

---
+++ sdk/next/index.mjs
@@ -0,0 +1,37 @@
+export {
+	ACTIVATION_MODE,
+	CURRENT_ADAPTER_DESCRIPTOR_CONTRACT,
+	SDK_ENTRYPOINT_PROTOCOL,
+	SDK_MANIFEST_CONTRACT,
+	planExternalAdapterActivation,
+	supportsBskelVersion,
+	validateAdapterSdkManifest,
+} from './manifest.mjs';
+
+export {
+	SDK_OPERATIONS,
+	makeSdkRequest,
+	validateSdkRequest,
+	validateSdkResponse,
+} from './protocol.mjs';
+
+export {
+	SUPPORT_EXPLANATION_CONTRACT,
+	SUPPORT_STATUSES,
+	createSupportExplanation,
+	renderSupportExplanationMarkdown,
+} from './explain.mjs';
+
+export { diagnosticsToSarif, SARIF_VERSION } from './sarif.mjs';
+
+export {
+	TASK_PACKET_CONTRACT,
+	createAdapterTaskPacket,
+	renderTaskPacketMarkdown,
+	validateAdapterTaskPacket,
+} from './task-packet.mjs';
+
+export {
+	CONFORMANCE_REPORT_CONTRACT,
+	runAdapterSdkConformance,
+} from './testkit.mjs';
