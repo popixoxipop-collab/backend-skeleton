48784ee51924cab766dcdfaaeefcf8f9aaaada2d
popixoxipop-collab <popixoxipop@gmail.com>
2026-09-25 11:57:24 +0900

    test(t12): add Django DRF conformance router

---
+++ test/fixtures/python-django/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet, basename='user')
+urlpatterns = [path('api/', include(router.urls))]
